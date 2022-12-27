daf9f3af6f1ee232e55e05459e824d5a853c241c
Taka <okawatakanori9916@gmail.com>
2022-12-28 00:28:44 +0900

    ジェネリクスのユースケース

--- pages/components/Generics.tsx
+++ pages/components/Generics.tsx
@@ -1,13 +1,79 @@
+import { useState } from "react"
+
 type Foo<T> = {
   foo: T
 }
 
+// Userデータの型を後から限定したいという用件の場合
+// ベースの型
+type User<T> = {
+  name: string
+  city: T
+}
+
+// 日本仕様
+type Japanese = User<"大阪府" | "東京都" | "愛知県" | "福岡県">
+
+// アメリカ仕様
+type American = User<"NY" | "LOS">
+
+
+const jpUsers: Japanese[] = []
+const usUsers: American[] = []
+
+// 日本ユーザー作成
+const jpUser: Japanese = {
+  name: "伊藤",
+  city: "愛知県"
+}
+
+// アメリカユーザー作成
+const usUser: American = {
+  name: "ジョン",
+  city: "NY"
+}
+
+// 直接指定
+const user: User<"日本" | "日本以外"> = {
+  name: "田中",
+  city: "日本"
+}
+
 export const Generics = () => {
+  console.log("rendering🌟")
+
+  const [name, setName] = useState("")
+  const [city, setCity] = useState("")
 
   const CheckVal = (obj: Foo<string>) => {
     console.log(obj)
   }
+
+  // const register = () => {
+  //   if(city) return;
+  //   usUsers.push({name: name, city: city})
+  // }
+
+  // const show = () => {
+  //   console.log("日本", jpUsers)
+  //   console.log("アメリカ", usUsers)
+  // }
+
   return (
-    <button onClick={() => CheckVal({foo: "文字列のみを送る"})}>ジェネリクス</button>
+    <>
+      <button onClick={() => CheckVal({foo: "文字列のみを送る"})}>文字列の型を送る</button>
+
+      <br />
+      <label>名前</label>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+
+      <br />
+      <label>都市</label>
+      <input type="text" value={city} onChange={(e) => setCity(e.target.value)} />
+      <button>登録</button>
+
+      {/* <button onClick={() => register()}>ユーザー登録</button>
+      <button onClick={() => show()}>ユーザー一覧</button> */}
+    </>
   )
 }
