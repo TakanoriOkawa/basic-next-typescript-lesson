e79bae241e546d016cdc738ee4b9121567770fa5
Taka <okawatakanori9916@gmail.com>
2022-12-28 01:58:33 +0900

    ジェネリクス関数の型制限

--- pages/utility/Generics.ts
+++ pages/utility/Generics.ts
@@ -8,3 +8,19 @@ export const product: ProductList<string, number> = {
   name: "机",
   price: 10000
 }
+
+// ジェネリクスを使った関数
+
+// 注意点としては、extendsが無いと、argがunknownになるようだ。
+// 試す
+
+// アロー関数の書き方
+// 関数名 = <Generic>(arg:Generic) => {}
+const upperString = <T extends string>(arg: T) => {
+  console.log(arg.toUpperCase())
+}
+
+// ジェネリクスを指定する場合
+upperString<string>("hoge");
+// 引数から型を推論してくれる
+upperString("fuga")
