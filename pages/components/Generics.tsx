7fb86631e2d8ae514d23067aa58bab03a1903577
Taka <okawatakanori9916@gmail.com>
2022-12-28 19:09:03 +0900

    テスト

--- pages/components/Generics.tsx
+++ pages/components/Generics.tsx
@@ -35,7 +35,7 @@ const usUser: American = {
 }
 
 // 直接指定
-const user: User<"日本" | "日本以外"> = {
+const literalUser: User<"日本" | "日本以外"> = {
   name: "田中",
   city: "日本"
 }
@@ -44,11 +44,23 @@ export const Generics = () => {
   const [name, setName] = useState("")
   const [city, setCity] = useState("");
 
-  // まだ完璧に理解できていない
-  const [users, setUsers] = useState<Japanese[] | American[]>([])
+  // ＊＊まだ完璧に理解できていない。ジェネリクスのUnion型配列にした場合、setUsersで値を格納できない。
+  const [users, setUsers] = useState<Japanese[]>([])
 
   const CheckVal = (obj: Foo<string>) => {
     console.log(obj)
+  }
+
+  const register = (name: string , city: string) => {
+    // あまり良い実装では無いが、特定の文字列で条件分岐させる。（最初から自由に記述できるtextfieldではなく、選択肢形式にした方が良い。）
+    if(city === "大阪府" || city === "東京都" || city === "愛知県" || city === "福岡県") {
+      // 値の追加
+      setUsers(prev => [...prev, {name: name, city: city}])
+      console.log("登録成功")
+      return
+    }
+
+    console.log("登録失敗")
   }
 
   const show = () => {
@@ -67,9 +79,16 @@ export const Generics = () => {
       <br />
       <label>都市</label>
       <input type="text" value={city} onChange={(e) => setCity(e.target.value)} />
-      <button>登録</button>
 
+      <button onClick={() => register(name, city)}>登録</button>
       <button onClick={() => show()}>ユーザー一覧</button>
+
+      {
+        users.map((val) => (
+          // 同名のユーザーがいた場合エラーになる
+          <p key={val.name}>{val.name}: {val.city}</p>
+        ))
+      }
     </>
   )
 }
